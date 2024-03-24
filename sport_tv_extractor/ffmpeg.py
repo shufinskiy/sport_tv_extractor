1a96160b45c8855dee2b8d7f66769125ca520fd5
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2024-03-24 15:44:15 +0300

    add typing for ffmpeg file

--- sport_tv_extractor/ffmpeg.py
+++ sport_tv_extractor/ffmpeg.py
@@ -1,11 +1,27 @@
 import re
-import subprocess
+import subprocess as sp
 from pathlib import Path
+from typing import Optional, List
 
 import numpy as np
 
 
 class FFMpeg(object):
+    """
+    Attributes:
+        path
+        img_dir
+        video_dir
+        output_name
+        device
+        verbose
+        rm_img
+        rm_video
+        codec
+        num_frame
+        fps
+        bitrate
+    """
 
     def __init__(self,
                  path: str,
@@ -25,24 +41,43 @@ class FFMpeg(object):
         self.verbose = verbose_mode
         self.rm_img = rm_tmp_image
         self.rm_video = rm_tmp_video
-        self.codec = self._get_codec()
-        self.num_frame = None
-        self.fps = None
-        self.bitrate = None
+        self.codec: List[str] = self._get_codec()
+        self.num_frame: Optional[int] = None
+        self.fps: Optional[int] = None
+        self.bitrate: Optional[float] = None
         Path.cwd().joinpath(self.img_dir).mkdir(parents=True, exist_ok=True)
         Path.cwd().joinpath(self.video_dir).mkdir(parents=True, exist_ok=True)
 
     def _get_codec(self):
+        """
+
+        Returns:
+
+        """
         if self.device == 'cpu':
             return ['', '-c:v libx264']
         else:
             return ['-c:v h264_cuvid', '-c:v h264_cuvid']
 
     def cut_frames(self) -> None:
-        cmd = f'ffmpeg {self.codec[0]} -i {self.path} -vf fps=1 -qscale:v 2 {self.verbose} {self.img_dir}/img-%02d.jpeg'
-        self._call_subprocess(cmd)
+        """
 
-    def loop_cut_frames(self, img_dir, arr_frame: np.ndarray) -> None:
+        Returns:
+
+        """
+        cmd = f'ffmpeg {self.codec[0]} -i {self.path} -vf fps=1 -qscale:v 2 {self.verbose} {self.img_dir}/img-%02d.jpeg'
+        self._call_sp(cmd)
+
+    def loop_cut_frames(self, img_dir: str, arr_frame: np.ndarray) -> None:
+        """
+
+        Args:
+            img_dir:
+            arr_frame:
+
+        Returns:
+
+        """
         cmd = "".join([
             f'arr=({" ".join([str(x) for x in arr_frame])}); '
             'for i in "${arr[@]}"; do ffmpeg ',
@@ -53,58 +88,93 @@ class FFMpeg(object):
             f'{str(img_dir)}/',
             '"img-${i%.*}".jpeg; done;'
         ])
-        subprocess.call(cmd, shell=True, executable='/bin/bash')
+        sp.call(cmd, shell=True, executable='/bin/bash')
 
     def cut_videos(self,
                    start_time: float,
                    duration: float,
                    idx_video: int
                    ) -> None:
+        """
+
+        Args:
+            start_time:
+            duration:
+            idx_video:
+
+        Returns:
+
+        """
         if self.device == 'cpu':
             cmd = f'ffmpeg -ss {start_time} -t {duration} -i {self.path} -vf "setpts=PTS-STARTPTS" {self.codec[1]} -crf 21 -preset ultrafast {self.verbose} -an {self.video_dir}/video_{idx_video}.mkv'
         else:
             cmd = f'ffmpeg {self.codec[1]} -hwaccel cuvid -ss {start_time} -t {duration} -i {self.path} -vf "setpts=PTS-STARTPTS" -c:v h264_nvenc -b:v {self.bitrate_video()}M -preset "hq" {self.verbose} -an -y {self.video_dir}/video_{idx_video}.mkv'
-        self._call_subprocess(cmd)
+        self._call_sp(cmd)
 
     def concat_videos(self) -> None:
+        """
+
+        Returns:
+
+        """
         cmd = f'ffmpeg -f concat -safe 0 -i {self.video_dir}/file.txt -c:v copy {self.verbose} {self.output_name}'
-        self._call_subprocess(cmd)
+        self._call_sp(cmd)
 
     def rm_tmp_files(self) -> None:
+        """
+
+        Returns:
+
+        """
         if self.rm_img:
             cmd = f'rm -rf {self.img_dir}'
-            self._call_subprocess(cmd)
+            self._call_sp(cmd)
         if self.rm_video:
             cmd = f'rm -rf {self.video_dir}'
-            self._call_subprocess(cmd)
+            self._call_sp(cmd)
 
-    def get_num_frame(self):
+    def get_num_frame(self) -> int:
+        """
+
+        Returns:
+
+        """
         if self.num_frame is None:
-            p = subprocess.Popen(['ffprobe', '-v', 'error', '-select_streams',
+            p = sp.Popen(['ffprobe', '-v', 'error', '-select_streams',
                                   'v:0', '-count_packets', '-show_entries',
                                   'stream=nb_read_packets', '-of', 'csv=p=0',
                                   self.path.replace('\ ', ' ')],
-                                 stdout=subprocess.PIPE)
+                                 stdout=sp.PIPE)
             out, err = p.communicate()
             self.num_frame = int(out.decode('utf-8').strip('\n'))
         return self.num_frame
 
-    def get_fps(self):
+    def get_fps(self) -> int:
+        """
+
+        Returns:
+
+        """
         if self.fps is None:
-            p = subprocess.Popen(['ffprobe', '-v', 'error', '-select_streams',
+            p = sp.Popen(['ffprobe', '-v', 'error', '-select_streams',
                                   'v:0', '-show_entries', 'stream=avg_frame_rate',
                                   '-of', 'default=noprint_wrappers=1:nokey=1',
                                   self.path.replace('\ ', ' ')],
-                                 stdout=subprocess.PIPE)
+                                 stdout=sp.PIPE)
             out, err = p.communicate()
             self.fps = int(out.decode('utf-8').strip('\n').split('/')[0])
         return self.fps
 
-    def bitrate_video(self):
+    def bitrate_video(self) -> float:
+        """
+
+        Returns:
+
+        """
         if self.bitrate is None:
 
             cmd = ['ffprobe', '-i', self.path.replace('\ ', ' ')]
-            pipe = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=10 ** 5)
+            pipe = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE, bufsize=10 ** 5)
             meta = pipe.stderr.read().decode('utf-8').split('\n')
             v_bt = [re.search(r'(?<=bitrate:\s)\d+\s+.+', line).group() for line in meta if
                     re.search(r'bitrate:\s+\d+\s+.+', line) is not None]
@@ -127,5 +197,13 @@ class FFMpeg(object):
             return self.bitrate
 
     @staticmethod
-    def _call_subprocess(command: str) -> None:
-        subprocess.call(command, shell=True)
+    def _call_sp(command: str) -> None:
+        """
+
+        Args:
+            command:
+
+        Returns:
+
+        """
+        sp.call(command, shell=True)
